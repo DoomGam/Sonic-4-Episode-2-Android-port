4ffe6ab1c1324859e7cab0a4d6d18907ba6f00c6
DoomGam <kevinmarques043@gmail.com>
2026-09-06 21:26:45 -0300

    Modify GLESBridge initGL method signature
    Changed initGL method signature from bool to void and updated related function declarations.

--- app/src/main/cpp/bridge/gles_bridge.h
+++ app/src/main/cpp/bridge/gles_bridge.h
@@ -1,30 +1,19 @@
 #ifndef GLES_BRIDGE_H
 #define GLES_BRIDGE_H
 
-#include <EGL/egl.h>
-#include <GLES2/gl2.h>
-
 class GLESBridge {
 public:
     static GLESBridge& getInstance();
-
-    bool initGL();
+    void initGL();
     void resize(int width, int height);
     void renderFrame();
-    void destroyGL();
-
 private:
     GLESBridge() = default;
     ~GLESBridge() = default;
-
-    int screenWidth = 0;
-    int screenHeight = 0;
-    bool initialized = false;
 };
 
 extern "C" {
-    void* fake_EAGLContext_setCurrentContext(void* context);
-    void fake_glSwapBuffers();
+    bool fake_EAGLContext_setCurrentContext(void* context);
 }
 
 #endif // GLES_BRIDGE_H
