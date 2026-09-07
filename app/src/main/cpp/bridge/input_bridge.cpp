30e13fdf99a330f357c96390b23e8001a8f5f164
DoomGam <kevinmarques043@gmail.com>
2026-09-06 21:29:34 -0300

    Refactor input_bridge.cpp for logging and touch events
    Updated logging tag and simplified touch event handling.

--- app/src/main/cpp/bridge/input_bridge.cpp
+++ app/src/main/cpp/bridge/input_bridge.cpp
@@ -1,7 +1,7 @@
 #include "input_bridge.h"
 #include <android/log.h>
 
-#define LOG_TAG "Input_Bridge"
+#define LOG_TAG "InputBridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 InputBridge& InputBridge::getInstance() {
@@ -10,20 +10,9 @@ InputBridge& InputBridge::getInstance() {
 }
 
 void InputBridge::sendTouchEvent(int id, float x, float y, int phase) {
-    LOGI("Evento de Toque Android -> ID: %d | X: %.2f | Y: %.2f | Fase: %d", id, x, y, phase);
+    (void)id; (void)x; (void)y; (void)phase;
 }
 
-void InputBridge::sendButtonState(uint32_t buttonMask, bool isPressed) {
-    if (isPressed) {
-        currentButtonState |= buttonMask;
-    } else {
-        currentButtonState &= ~buttonMask;
-    }
-    LOGI("Estado do Gamepad Atualizado: 0x%X", currentButtonState);
-}
-
-extern "C" {
-    void fake_registerTouchEvent(int id, float x, float y, int phase) {
-        InputBridge::getInstance().sendTouchEvent(id, x, y, phase);
-    }
+extern "C" void fake_registerTouchEvent(int id, float x, float y, int phase) {
+    InputBridge::getInstance().sendTouchEvent(id, x, y, phase);
 }
