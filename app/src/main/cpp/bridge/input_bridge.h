5a8f41016447a3bd36aa77c8ea3ef7b4cbfedce8
DoomGam <kevinmarques043@gmail.com>
2026-09-06 21:29:07 -0300

    Clean up input_bridge.h by removing unused types
    Removed unused TouchPhase enum and TouchEventData struct.

--- app/src/main/cpp/bridge/input_bridge.h
+++ app/src/main/cpp/bridge/input_bridge.h
@@ -1,34 +1,13 @@
 #ifndef INPUT_BRIDGE_H
 #define INPUT_BRIDGE_H
 
-#include <cstdint>
-
-enum TouchPhase {
-    TOUCH_BEGAN = 0,
-    TOUCH_MOVED = 1,
-    TOUCH_ENDED = 2,
-    TOUCH_CANCELLED = 3
-};
-
-struct TouchEventData {
-    int id;
-    float x;
-    float y;
-    TouchPhase phase;
-};
-
 class InputBridge {
 public:
     static InputBridge& getInstance();
-
     void sendTouchEvent(int id, float x, float y, int phase);
-    void sendButtonState(uint32_t buttonMask, bool isPressed);
-
 private:
     InputBridge() = default;
     ~InputBridge() = default;
-
-    uint32_t currentButtonState = 0;
 };
 
 extern "C" {
