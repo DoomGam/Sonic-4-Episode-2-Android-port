6f81ecb4f232632fed7c4be8837fc68fead6a560
DoomGam <kevinmarques043@gmail.com>
2026-09-06 21:28:04 -0300

    Refactor AudioBridge class and clean up includes
    Removed unused includes and private member variables from AudioBridge class. Updated method signatures for consistency.

--- app/src/main/cpp/bridge/audio_bridge.h
+++ app/src/main/cpp/bridge/audio_bridge.h
@@ -1,32 +1,18 @@
 #ifndef AUDIO_BRIDGE_H
 #define AUDIO_BRIDGE_H
 
-#include <SLES/OpenSLES.h>
-#include <SLES/OpenSLES_Android.h>
-
 class AudioBridge {
 public:
     static AudioBridge& getInstance();
-
-    bool initAudio();
-    void playSound(const char* soundPath);
-    void stopAllSounds();
-    void destroyAudio();
-
+    void initAudio();
 private:
     AudioBridge() = default;
     ~AudioBridge() = default;
-
-    SLObjectItf engineObject = nullptr;
-    SLEngineItf engineInterface = nullptr;
-    SLObjectItf outputMixObject = nullptr;
-
-    bool initialized = false;
 };
 
 extern "C" {
-    void fake_AudioServicesPlaySystemSound(uint32_t soundID);
-    void fake_alSourcePlay(uint32_t source);
+    void fake_AudioServicesPlaySystemSound(unsigned int soundID);
+    void fake_alSourcePlay(unsigned int source);
 }
 
 #endif // AUDIO_BRIDGE_H
